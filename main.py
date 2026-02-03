a3a3f207ca47cedbc92db33b01550da247f4bd04
anucha <anuchaalayaden17#gmail.com>
2026-02-03 10:07:16 +0100

    update

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ from BudgetTracker import BudgetTracker
 from Menu import Menu
 # Here I import the core classes.
 # This is the main file that starts the entire application
-# It creates the main objects and connects all parts together
 def main():
 
     # here i Create a Storage object to handle saving and loading data from data.json
     storage = Storage("data.json")
 
     # Creates a BudgetTracker object which controls the program logic
-    # It receives the storage object so it can save and load information
     tracker = BudgetTracker(storage)
 
     # Creates the Menu object to interact with the user
-    # The menu uses the tracker to perform actions
     menu = Menu(tracker)
 
     # Starts the program
